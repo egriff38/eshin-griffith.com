2ce6b789c1ace12d690c194d353200565638139a
Eshin Griffith <eshin@me.com>
2024-07-27 14:15:50 -0400

    Added astro + netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig, passthroughImageService } from "astro/config";
-
 import icon from "astro-icon";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
   image: {
     domains: ["res.cloudinary.com"],
-    service: passthroughImageService(),
+    service: passthroughImageService()
   },
+  output: "server",
+  adapter: netlify()
 });
